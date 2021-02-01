b0116ab70dd82ca442a2af69dce971b86d8fe816
Eric Segala Jovelli <ericsegalajovelli@gmail.com>
2021-02-01 01:02:20 -0300

    added dashboard and users tab

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import * as React from "react";
-import { Admin } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
+import UserIcon from '@material-ui/icons/Group';
+import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
+import Dashboard from './Dashboard';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-const App = () => <Admin dataProvider={dataProvider} />;
-
+const App = () => (<Admin dashboard={Dashboard} dataProvider={dataProvider}>
+    <Resource name="users" list={UserList} icon={UserIcon} />
+</Admin>
+);
 export default App;
---
+++ src/Dashboard.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { Card, CardContent, CardHeader } from '@material-ui/core';
+
+export default () => (
+    <Card>
+        <CardHeader title="Welcome to administration!" />
+        <CardContent>
+            <h1>Guide</h1>
+            <h2>About</h2>
+            <p>This is an administration platform for photo uploading websites, 
+                like Piterest and Pixabay. Using the menu buttons on the left, you can
+                select some entity (like users, comments...) to
+                perform the following operations:</p>
+                <ul>
+                    <li>Insertion</li>
+                    <li>Search</li>
+                    <li>Edition</li>
+                    <li>Deletion</li>
+                </ul>
+                <p>Below you can check more details about each menu tab:</p>
+            <h3>Users</h3>
+            <p>Users registered in this platform</p>
+            <h3>Albums</h3>
+            <p>Albums of photos uploaded by the users</p>
+            <h3>Photos</h3>
+            <p>Photos posted by the users</p>
+            <h3>Comments</h3>
+            <p>Comments posted on photos</p>
+        </CardContent>
+    </Card>
+);
